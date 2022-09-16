c49be2d96d79f69b6d46986b04259499b34e2ce7
Beker-Dev <joaopedro.beker@hotmail.com>
2022-09-16 16:37:41 -0300

    added pagination

--- vehicles/admin/vehicle_admin.py
+++ vehicles/admin/vehicle_admin.py
@@ -1,12 +1,34 @@
+import rest_framework.pagination
 from django.contrib import admin
 from vehicles.models import Vehicle
 
 
 @admin.register(Vehicle)
 class VehicleAdmin(admin.ModelAdmin):
-    list_display = ['id', 'model', 'mileage', 'price', 'year', 'is_new', 'fuel', 'engine',
-                    'engine_type',  'automatic_transmission',
-                    'gears', 'type'
-                    ]
-    list_display_links = ['id', 'model']
-    list_filter = ['model__brand', 'is_new', 'fuel', 'engine', 'engine_type', 'automatic_transmission', 'type']
+    list_display = [
+        'id',
+        'model',
+        'mileage',
+        'price',
+        'year',
+        'is_new',
+        'fuel',
+        'engine',
+        'engine_type',
+        'automatic_transmission',
+        'gears',
+        'type'
+    ]
+    list_display_links = [
+        'id',
+        'model'
+    ]
+    list_filter = [
+        'model__brand',
+        'is_new',
+        'fuel',
+        'engine',
+        'engine_type',
+        'automatic_transmission',
+        'type'
+    ]
--- vehicles/views/api/v1/vehicles_view_set.py
+++ vehicles/views/api/v1/vehicles_view_set.py
@@ -1,10 +1,12 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from vehicles.models import Vehicle
 from vehicles.serializers import VehicleSerializer
+from rest_framework.pagination import PageNumberPagination
 
 
 class VehiclesViewSet(ReadOnlyModelViewSet):
     queryset = Vehicle.objects.get_active()
     serializer_class = VehicleSerializer
     http_method_names = ['get', 'options', 'head']
+
 
