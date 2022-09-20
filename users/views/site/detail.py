0e35c7eec25503205c587699d4ef025ba7fb9a38
Beker-Dev <joaopedro.beker@hotmail.com>
2022-09-20 16:30:38 -0300

    fixed detail security

--- users/views/site/detail.py
+++ users/views/site/detail.py
@@ -1,11 +1,14 @@
 from django.views.generic import DetailView
 from django.contrib.auth.models import User
-from users.forms import RegisterForm, LoginForm
+from django.shortcuts import redirect
 
 
 class Detail(DetailView):
-    slug_url_kwarg = 'username'
-    slug_field = 'username'
     model = User
     template_name = 'users/detail.html'
     context_object_name = 'user'
+
+    def dispatch(self, *args, **kwargs):
+        if self.kwargs.get('pk') != self.request.user.pk:
+            return redirect('vehicles:list')
+        return super().dispatch(*args, **kwargs)
