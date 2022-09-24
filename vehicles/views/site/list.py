ec35e12318b6006bf0a83b980d985d813b014af2
Beker-Dev <joaopedro.beker@hotmail.com>
2022-09-24 19:43:47 -0300

    made some filters

--- vehicles/views/site/list.py
+++ vehicles/views/site/list.py
@@ -10,3 +10,14 @@ class List(ListView):
     paginate_by = 20
     ordering = ['-id']
     queryset = Vehicle.objects.get_active_vehicles()
+
+    def check_filters(self):
+        allowed_filters = ['type']
+        for filter, value in self.request.GET.items():
+            if filter in allowed_filters:
+                self.queryset = self.queryset.filter(**{filter: value})
+        return self.queryset
+
+    def get_queryset(self):
+        qs = self.check_filters()
+        return qs
