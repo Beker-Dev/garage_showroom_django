a90418821aef8c8b5a8fe0d1de4624c537d0e305
Beker-Dev <joaopedro.beker@hotmail.com>
2022-09-27 12:04:04 -0300

    added user register form validations

--- users/forms/register_form.py
+++ users/forms/register_form.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class RegisterForm(forms.ModelForm):
@@ -60,6 +61,49 @@ class RegisterForm(forms.ModelForm):
             }
         )
     )
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        required_length = 6
+        if len(username) < required_length:
+            raise ValidationError(f'Username must have at least {required_length} characters', code='min_length')
+        return username
+
+    def clean_password(self):
+        password = str(self.cleaned_data.get('password'))
+        required_length = 8
+        has_uppercase = False
+        has_lowercase = False
+        has_digit = False
+        has_space = False
+        if len(password) < required_length:
+            raise ValidationError(f'Password must have at least {required_length} characters', code='min_length')
+        else:
+            for c in password:
+                if c.islower():
+                    has_lowercase = True
+                if c.isupper():
+                    has_uppercase = True
+                if c.isdigit():
+                    has_digit = True
+                if c.isspace():
+                    has_space = True
+            else:
+                if not has_lowercase or not has_uppercase or not has_digit or has_space:
+                    raise ValidationError(
+                            f'Password must have at least one lowercase, uppercase, digit and no '
+                            f'spaces', code='invalid'
+                        )
+                else:
+                    return password
+
+    def clean(self):
+        cleaned_data = super().clean()
+        pw = cleaned_data.get('password')
+        pw2 = cleaned_data.get('password2')
+        pw_error = 'Password must be equal'
+        if pw != pw2:
+            raise ValidationError({'password': pw_error, 'password2': pw_error})
 
     class Meta:
         model = User
